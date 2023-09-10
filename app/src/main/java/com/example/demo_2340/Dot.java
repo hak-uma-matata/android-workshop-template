86fa7c6a6b2c083e6d13603bdd5a60c53064519b
sal5121 <saloni.lr.rath@gmail.com>
2023-09-10 18:55:31 -0400

    implementing task 1/2

--- app/src/main/java/com/example/demo_2340/Dot.java
+++ app/src/main/java/com/example/demo_2340/Dot.java
@@ -16,11 +16,13 @@ public class Dot {
         this.isVisible = true;
 
         // TODO Record the time when the dot becomes visible
+        this.visibleStartTime = System.currentTimeMillis();
 
     }
 
     public boolean isExpired() {
         // TODO Check if the dot has exceeded its max lifetime
+        return System.currentTimeMillis() - visibleStartTime >= MAX_LIFETIME;
     }
 
     public float getX() {
